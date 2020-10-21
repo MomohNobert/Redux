63fee71d44238efc341f1e2e0a3330be319e9fc4
Momoh Nobert <okhai0112@gmail.com>
2020-10-21 09:23:49 +0100

    added connect and reset component

--- react-redux-demo/src/components/CakeContainer.js
+++ react-redux-demo/src/components/CakeContainer.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { buyCake } from '../redux';
 
-function CakeContainer() {
+
+function CakeContainer({ buyCake, numOfCakes }) {
     return (
         <div>
-            <h2>Number of Cakes.</h2>
-            <button>Buy Cakes</button>
+            <h2>Number of Cakes - {numOfCakes} </h2>
+            <button onClick={buyCake}>Buy Cakes</button>
         </div>
     )
 }
 
-export default CakeContainer
+const mapStateToProps = state => {
+    return {
+        numOfCakes: state.numOfCakes
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        buyCake: () => dispatch(buyCake())
+    }
+}
+
+export default connect(
+    mapStateToProps, 
+    mapDispatchToProps)
+    (CakeContainer)
 
